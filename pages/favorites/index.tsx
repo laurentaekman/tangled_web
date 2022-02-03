f22486d54d3a767a51389ef960273b822bcb2ec6
Lauren Taekman <lauren.taekman@willowtreeapps.com>
2022-02-03 09:16:38 -0500

    Adding default display when no favorites are available

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -12,15 +12,25 @@ const Favorites = () => {
       <Header />
       <div className={styles.favorites}>
         <h2>Favorites</h2>
-        <div>{`Navigate to a favorited art post by clicking the row's arrow icon.`}</div>
-        <div>{`Unfavorite an art post by clicking the row's heart icon.`}</div>
-        {favorites.map((favorite: ArtObject) => (
-          <FavoritesRow
-            item={favorite}
-            removeFavorite={removeFavorite}
-            key={favorite.id}
-          />
-        ))}
+        {favorites.length > 0 && (
+          <main>
+            <div>{`Navigate to a favorited art post by clicking the row's arrow icon.`}</div>
+            <div>{`Unfavorite an art post by clicking the row's heart icon.`}</div>
+            {favorites.length > 0 &&
+              favorites.map((favorite: ArtObject) => (
+                <FavoritesRow
+                  item={favorite}
+                  removeFavorite={removeFavorite}
+                  key={favorite.id}
+                />
+              ))}
+          </main>
+        )}
+        {favorites.length <= 0 && (
+          <main>
+            <div>No favorites yet!</div>
+          </main>
+        )}
       </div>
     </div>
   );
